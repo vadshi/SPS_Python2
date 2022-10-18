ad58ea03165c945f7988646f899cdf3798a85414
Vadim <vadimx30@gmail.com>
2022-10-18 20:47:46 +0300

    update Day2

--- Day_2/Solutions/sol_00_color_points.py
+++ Day_2/Solutions/sol_00_color_points.py
@@ -1,17 +1,23 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        self.color = ...
+        self.color = color
 
     def dist_to(self, other_point):
-        ...
+        return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
 
 
-p1 = Point(4, 4)
-p2 = Point(3, 3)
 
-result = p1.dist_to(p2)
+def geron(pnts):
+    """ Вычисляем площадь по формуле Герона."""
+    a = pnts[0].dist_to(pnts[1])
+    b = pnts[1].dist_to(pnts[2])
+    c = pnts[2].dist_to(pnts[0])
+    p = (a + b + c) / 2
+    return (p * (p - a) * (p - b) * (p - c)) ** 0.5
+
+
 
 # Дан список точек нарисованных красным(red) и зеленым(green) цветами
 # Точно известно что точек каждого цвета ровно три,
@@ -30,7 +36,14 @@ points = [
 # Задание-2: реализуйте метод dist_to()
 # Задание-3: вычислите площади треугольников образованных из точек одного цвета(зеленый и красный)
 
-# TODO: your core here...
+greens = []
+reds = []
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+for point in points:
+    if point.color == 'red':
+        reds.append(point)
+    else:
+        greens.append(point)
+
+print("Площадь красного треугольника = ", geron(reds))
+print("Площадь зеленого треугольника = ", geron(greens))
