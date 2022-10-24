8ad59940fc18537d157b001264112a9791c893f7
Alex-N1 <90287524+Alex-N1@users.noreply.github.com>
2022-10-24 18:42:39 +0300

    Update 01_letters.py

--- Day_5/Homework/01_letters.py
+++ Day_5/Homework/01_letters.py
@@ -1,10 +1,13 @@
 """
-Напишите генератор letters(some_str), выдает только буквы из 
+Напишите генератор letters(some_str), выдает только буквы из
 входящей строки(остальные игнорируются).
 """
-def letters(some_str):
-	pass
+import re
 
+def letters(some_str):
+    for letter in some_str:
+        if re.match(r'[a-zA-Z]', letter):
+            yield(letter)
 
 output = letters('A!sdf 09 _ w')
 print(next(output))  # Out: 'A'
